2b719cdc95069193d2a7165e5fba1bdad6d01522
Travis Watson <watsonjamest@gmail.com>
2019-07-17 12:08:31 -0500

    Add ability to edit and update grams

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -8,6 +8,23 @@ class GramsController < ApplicationController
         @gram = Gram.new
     end
 
+    def update
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+
+        @gram.update_attributes(gram_params)
+        if @gram.valid?
+            redirect_to root_path
+        else
+            return render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def edit
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+    end
+
     def create
         @gram = current_user.grams.create(gram_params)
         if @gram.valid?
@@ -17,10 +34,19 @@ class GramsController < ApplicationController
         end
     end
 
+    def show
+        @gram = Gram.find_by_id(params[:id])
+        return render_not_found if @gram.blank?
+    end
+
     private
 
     def gram_params
         params.require(:gram).permit(:message)
+    end
+
+    def render_not_found
+        render plain: 'Not Found :(', status: :not_found
     end
 
 end
