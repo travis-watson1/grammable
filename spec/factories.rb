937a608a9a3fc1ef157d851947a18ad74c66af9f
Travis Watson <watsonjamest@gmail.com>
2019-07-22 13:33:57 -0500

    Add comments to site

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,8 @@
 FactoryBot.define do
+  factory :comment do
+    
+  end
+
     factory :user do
       sequence :email do |n|
          "dummyEmail#{n}@gmail.com" 
@@ -9,6 +13,7 @@ FactoryBot.define do
 
     factory :gram do
       message { "hello" }
+      picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
       association :user
     end
   end
