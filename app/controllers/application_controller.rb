a78cb0f0f0f2974a5a64b952db9f08ea3bd7fd32
Travis Watson <watsonjamest@gmail.com>
2019-07-22 14:11:00 -0500

    Style app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+
+    def render_not_found(status=:not_found)
+      render plain: "#{status.to_s.titleize} :(", status: status
+    end
+  
+
 end
